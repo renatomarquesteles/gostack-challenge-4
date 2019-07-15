0e63da88d8f9760df22b15fb9ac282583b0f058c
Renato <renatomarquesteles@gmail.com>
2019-07-15 03:03:55 -0300

    feature: Class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import profile from "./assets/profile.jpg";
+import TechList from './components/TechList';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <img width="200" src={profile} />;
+  return <TechList />;
 }
 
 export default App;
